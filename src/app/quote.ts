c3cf879de90780ac4d00a1e6ee39adde579d4fa3
b3nzuk3 <hussenito7@gmail.com>
2022-04-12 03:11:09 +0300

    adding form reset functionality

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,3 +1,3 @@
 export class Quote {
-  constructor(public id: number,public description: string, public name: string,public userName: string,public quoteTitle:string, public completeDate: Date, public likes: number, public dislikes: number){}
+  constructor(public id: number,public description: string, public name: string,public author: string,public title:string, public completeDate: Date, public likes: number, public dislikes: number){}
 }
