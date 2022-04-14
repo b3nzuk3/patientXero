ea04bfc830b4aec252b08750d3654ef0e4580a05
b3nzuk3 <hussenito7@gmail.com>
2022-04-14 10:30:53 +0300

    tweaks

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -9,32 +9,40 @@ import { Quote } from "../quote";
 })
 export class QuoteComponent implements OnInit {
   quotes: Quote[] = [
-    new Quote(1, 'Simplicity is the ultimate sophistication', 'Learnado Davinci','Anna','Science',new Date(2020,1,26), 0, 0),
-    new Quote(2, 'Art is chaos taking shape','Pablo Picasso','james','Art',new Date(2021,7,19),0,0),
-    new Quote(3, 'If life were predictable it would cease to be life, and be without flavor.', 'Eleanor Roosevelt','Alan','Life',new Date(2021,6,12),0,0),
-    new Quote(4, 'A sky full of stars and he was staring at her.', 'Atticus','Klaus','Romance',new Date(2021,4,23),0,0)]
+    new Quote(1, 'Simplicity is the ultimate sophistication', 'Learnado Davinci', 'Anna', 'Science', new Date(2020, 1, 26), 0, 0),
+    new Quote(2, 'Art is chaos taking shape', 'Pablo Picasso', 'james', 'Art', new Date(2021, 7, 19), 0, 0),
+    new Quote(3, 'If life were predictable it would cease to be life, and be without flavor.', 'Eleanor Roosevelt', 'Alan', 'Life', new Date(2021, 6, 12), 0, 0),
+    new Quote(4, 'A sky full of stars and he was staring at her.', 'Atticus', 'Klaus', 'Romance', new Date(2021, 4, 23), 0, 0)]
+
+  mostVoted = this.getVoted();
+  getVoted() {
+    let quoteCopy = [...this.quotes]
+    return quoteCopy.sort(
+      (current: any, next: any) => next.likes - current.likes
+    )[0];
+  }
 
 
-    deleteQuote(isComplete, index){
-     if (isComplete) {
-       let toDelete = confirm(`Are you sure you want to delete ${this.quotes[index].name}?`)
+  deleteQuote(isComplete, index) {
+    if (isComplete) {
+      let toDelete = confirm(`Are you sure you want to delete ${this.quotes[index].name}?`)
 
-       if (toDelete){
-         this.quotes.splice(index,1)
-       }
-     }
-   }
+      if (toDelete) {
+        this.quotes.splice(index, 1)
+      }
+    }
+  }
 
-   addNewQuote(quote){
-       let goalLength = this.quotes.length;
-       quote.id = goalLength+1;
-       quote.completeDate = new Date(quote.completeDate)
-       this.quotes.push(quote)
-     }
+  addNewQuote(quote) {
+    let goalLength = this.quotes.length;
+    quote.id = goalLength + 1;
+    quote.completeDate = new Date(quote.completeDate)
+    this.quotes.push(quote)
+  }
 
-     get Quote(){
-    return this.quotes.sort((a,b) =>{
-      return <any>new Date (b.completeDate) - <any>new Date(a.completeDate)
+  get Quote() {
+    return this.quotes.sort((a, b) => {
+      return <any>new Date(b.completeDate) - <any>new Date(a.completeDate)
     });
   }
 
