272c84f6558acdff047206330fc546f6b81bca5a
b3nzuk3 <hussenito7@gmail.com>
2022-04-14 15:29:32 +0300

    made the highlight directive and implemented it

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -9,10 +9,10 @@ import { Quote } from "../quote";
 })
 export class QuoteComponent implements OnInit {
   quotes: Quote[] = [
-    new Quote(1, 'Simplicity is the ultimate sophistication', 'Learnado Davinci', 'Anna', 'Science', new Date(2020, 1, 26), 5, 0),
-    new Quote(2, 'Art is chaos taking shape', 'Pablo Picasso', 'james', 'Art', new Date(2021, 7, 19), 6, -1),
-    new Quote(3, 'If life were predictable it would cease to be life, and be without flavor.', 'Eleanor Roosevelt', 'Alan', 'Life', new Date(2021, 6, 12), 0, 0),
-    new Quote(4, 'A sky full of stars and he was staring at her.', 'Atticus', 'Klaus', 'Romance', new Date(2021, 4, 23), 8, 0)]
+    new Quote(1, 'Simplicity is the ultimate sophistication', 'Learnado Davinci', 'Anna', 'Science', new Date(2020, 1, 26), 5, -78),
+    new Quote(2, 'Art is chaos taking shape', 'Pablo Picasso', 'james', 'Art', new Date(2021, 7, 19), 456, -1),
+    new Quote(3, 'If life were predictable it would cease to be life, and be without flavor.', 'Eleanor Roosevelt', 'Alan', 'Life', new Date(2021, 6, 12), 114, -34),
+    new Quote(4, 'A sky full of stars and he was staring at her.', 'Atticus', 'Klaus', 'Romance', new Date(2021, 4, 23), 48, -7)]
 
   mostVoted = this.getVoted();
   getVoted() {
